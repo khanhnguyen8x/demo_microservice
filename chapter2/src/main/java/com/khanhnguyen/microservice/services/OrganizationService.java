92fa87a8a9f20f8d5c3229d15c2e41d45b375f1b
Khanh Nguyen <khanh.nguyen.viet1989@gmail.com>
2020-02-05 11:15:46 +0700

    Import demo spmia

--- chapter2/src/main/java/com/khanhnguyen/microservice/services/OrganizationService.java
+++
@@ -1,7 +0,0 @@
-package com.khanhnguyen.microservice.services;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class OrganizationService {
-}
---
+++ spmia-chapter4/organization-service/src/main/java/com/thoughtmechanix/organization/services/OrganizationService.java
@@ -0,0 +1,41 @@
+package com.thoughtmechanix.organization.services;
+
+import com.thoughtmechanix.organization.model.Organization;
+import com.thoughtmechanix.organization.repository.OrganizationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class OrganizationService {
+    @Autowired
+    private OrganizationRepository orgRepository;
+
+    public List<Organization> getOrgs(){
+        List<Organization> data = new ArrayList<>();
+        orgRepository.findAll().forEach(data::add);
+        return data;
+    }
+
+    public Organization getOrg(String organizationId) {
+        return orgRepository.findById(organizationId);
+    }
+
+    public void saveOrg(Organization org){
+        org.setId( UUID.randomUUID().toString());
+
+        orgRepository.save(org);
+
+    }
+
+    public void updateOrg(Organization org){
+        orgRepository.save(org);
+    }
+
+    public void deleteOrg(Organization org){
+        orgRepository.delete( org.getId());
+    }
+}
